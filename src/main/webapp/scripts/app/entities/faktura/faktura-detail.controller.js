49800b5e79965e2b48d7d63f2ddf926c09996fdd
Saga <saga995@gmail.com>
2015-05-29 01:39:25 +0200

    wypisanie pozycji faktur na fakturze

--- src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
+++ src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('wzpnApp')
-    .controller('FakturaDetailController', function ($scope, $stateParams, Faktura, Przedsiebiorca, PozycjaFaktury) {
+    .controller('FakturaDetailController', function ($scope, $filter, $stateParams, Faktura, Przedsiebiorca, PozycjaFaktury) {
         $scope.faktura = {};
+        $scope.pozycjeFaktury = {};
         $scope.load = function (id) {
             Faktura.get({id: id}, function(result) {
               $scope.faktura = result;
+            });
+            
+         PozycjaFaktury.query(function(result) {
+            	var resultNew = [];
+            	angular.forEach(result, function(value, key) {
+            		if(value.faktura.id == id) resultNew.push(value);
+            	});
+            	
+            	 $scope.pozycjeFaktury = resultNew;
             });
         };
         $scope.load($stateParams.id);
