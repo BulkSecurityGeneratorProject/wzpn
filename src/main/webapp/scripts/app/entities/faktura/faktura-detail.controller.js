46fea8808f616c4b43d0bd44f5072aa740b104e1
Saga <saga995@gmail.com>
2015-06-02 00:27:22 +0200

    Wyglad faktury

--- src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
+++ src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
@@ -11,11 +11,19 @@ angular.module('wzpnApp')
             
          PozycjaFaktury.query(function(result) {
             	var resultNew = [];
+            	var totalMoney = 0;
+            	var podatek = 0;
             	angular.forEach(result, function(value, key) {
-            		if(value.faktura != null && value.faktura.id == id) resultNew.push(value);
+            		if(value.faktura != null && value.faktura.id == id) {
+            			resultNew.push(value);
+            			if(value.brutto!=null) totalMoney = totalMoney + value.brutto;
+            			if(value.podatek!=null)podatek = podatek + value.podatek;
+            		}
             	});
             	
             	 $scope.pozycjeFaktury = resultNew;
+            	 $scope.lacznaKwota = totalMoney;
+            	 $scope.lacznyPodatek = podatek; 
             });
         };
         $scope.load($stateParams.id);
