1ffae8d8ed54879d4873c36519e8bdee73448bfe
jakasnazwa <rafaelc@o2.pl>
2015-06-09 20:34:20 +0200

    Aktywacja guzikow dodawania i edycji wyniku w zaleznosci czy wynik istnieje

--- src/main/webapp/scripts/app/entities/grafik/grafik-detail.controller.js
+++ src/main/webapp/scripts/app/entities/grafik/grafik-detail.controller.js
@@ -4,9 +4,21 @@ angular.module('wzpnApp')
     .controller('GrafikDetailController', function ($scope, $stateParams,$filter, Grafik, Druzyna, ObiektSportowy, Wynik) {
         $scope.grafik = {};
         $scope.wyniks = Wynik.query();
+        $scope.isDisableAdd;
+        $scope.isDisableEdit;
         $scope.load = function (id) {
             Grafik.get({id: id}, function(result) {
               $scope.grafik = result;
+        	if ($scope.grafik.wynik != null)
+        		{
+        			$scope.isDisableAdd=true;
+        			$scope.isDisableEdit=false;
+        		}
+        	else
+        		{
+        		$scope.isDisableAdd=false;
+    			$scope.isDisableEdit=true;
+        		}
               console.log('=========== test ============='+ $scope.wynik);
               var found = $filter('filter')($scope.wyniks, {id: 1118}, true);
               console.log(found);
