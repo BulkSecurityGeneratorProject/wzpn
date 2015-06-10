a6ce6be133ff7853d42ae7dfa44851b37a632269
qbisiak <qbisiak@gmail.com>
2015-06-10 04:47:48 +0200

    Poprawione wyświetlanie faktury (format dat, liczb, liczenie bardziej przydantych wartości)

--- src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
+++ src/main/webapp/scripts/app/entities/faktura/faktura-detail.controller.js
@@ -12,18 +12,26 @@ angular.module('wzpnApp')
          PozycjaFaktury.query(function(result) {
             	var resultNew = [];
             	var totalMoney = 0;
+            	var totalMoneyVat = 0;
+            	var totalVat = 0;
             	var podatek = 0;
             	angular.forEach(result, function(value, key) {
             		if(value.faktura != null && value.faktura.id == id) {
             			resultNew.push(value);
-            			if(value.brutto!=null) totalMoney = totalMoney + value.brutto;
-            			if(value.podatek!=null)podatek = podatek + value.podatek;
+            			if(value.brutto!=null  && value.ilosc != null) totalMoneyVat = totalMoneyVat + (value.brutto * value.ilosc);
+            			if(value.brutto != null && value.ilosc != null && value.podatek != null) {
+            				totalMoney = totalMoney + (value.brutto * value.ilosc)/(100 - value.podatek)*100;
+            				totalVat = totalVat + value.brutto * value.ilosc * value.podatek / 100;
+            			}
+            			if(value.podatek!=null)	podatek = podatek + value.podatek;
             		}
             	});
             	
             	 $scope.pozycjeFaktury = resultNew;
             	 $scope.lacznaKwota = totalMoney;
+            	 $scope.lacznaKwotaVat = totalMoneyVat;
             	 $scope.lacznyPodatek = podatek; 
+            	 $scope.lacznyVat = totalVat;
             });
         };
         $scope.load($stateParams.id);
